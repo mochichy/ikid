42876b30aa4aab038dd090c77fa7156f2002c7ae
Christy Lu <hyl9@uw.edu>
2018-02-04 18:02:25 -0800

    second flip done

--- ikid/SecondViewController.swift
+++ ikid/SecondViewController.swift
@@ -10,6 +10,62 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var secondJoke: UITextView!
+    @IBOutlet weak var secondTitle: UILabel!
+    
+    fileprivate var secondFlipViewController : SecondFlipViewController!
+    
+    fileprivate func secondBuilder() {
+        if secondFlipViewController == nil {
+            secondFlipViewController =
+                storyboard?
+                    .instantiateViewController(withIdentifier: "SecondFlip")
+                as! SecondFlipViewController
+        }
+    }
+    
+    @IBAction func btnNext(_ sender: Any) {
+        switchViews()
+    }
+    
+    func switchViews() {
+        secondBuilder()
+        
+        UIView.beginAnimations("View Flip", context: nil)
+        UIView.setAnimationDuration(0.4)
+        UIView.setAnimationCurve(.easeInOut)
+        
+        if secondJoke.isHidden {
+            UIView.setAnimationTransition(.flipFromLeft, for: view, cache: true)
+            self.view.frame = view.frame
+            switchViewController(secondFlipViewController, to: nil)
+        } else {
+            UIView.setAnimationTransition(.flipFromLeft, for: view, cache: true)
+            secondFlipViewController.view.frame = view.frame
+            switchViewController(nil, to: secondFlipViewController)
+        }
+        
+        
+        UIView.commitAnimations()
+    }
+    
+    fileprivate func switchViewController(_ from: UIViewController?, to: UIViewController?) {
+        
+        if to != nil {
+            secondTitle.isHidden = true
+            secondJoke.isHidden = true
+            self.addChildViewController(to!)
+            self.view.insertSubview(to!.view, at: 0)
+            to!.didMove(toParentViewController: self)
+        } else {
+            secondJoke.isHidden = false
+            secondTitle.isHidden = false
+            from!.willMove(toParentViewController: nil)
+            from!.view.removeFromSuperview()
+            from!.removeFromParentViewController()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
